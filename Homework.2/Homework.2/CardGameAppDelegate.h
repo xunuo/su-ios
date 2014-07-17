c1fe32532e1f1dd46425bbe02f57e7dca1f0b8c4
nuo.xun <nuo.xun@alibaba-inc.com>
2014-07-17 17:07:49 +0800

    add homework.2

---
+++ Homework.2/Homework.2/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Homework.2
+//
+//  Created by snow on 14-7-17.
+//  Copyright (c) 2014年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homework.2/Homework.2/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Homework.2
+//
+//  Created by snow on 14-7-17.
+//  Copyright (c) 2014年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
